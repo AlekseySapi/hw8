a88bf623b8bde862a6466ada00c00cbbc0164898
Aleksey L <52339899+AlekseySapi@users.noreply.github.com>
2021-06-27 17:28:12 +0300

    8.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
+import java.util.Random;
+
 public class Main {
+
+    static long startTime, endTime;
 
     public static void main(String[] args) {
 
@@ -22,6 +26,31 @@ public class Main {
                 - Суммирование
                 - Возведение в степень
 */
+
+//        Задание 8.4
+        Item aDataItem;
+        int aKey;
+        int size = 50;
+
+
+        startTime = System.nanoTime();
+        HashTable hashTable = new HashTable(size);
+        Random rand = new Random();
+        for (int j = 0; j < 15; j++) {
+            aKey = rand.nextInt(500);
+            aDataItem = new Item(aKey);
+            hashTable.insert(aDataItem);
+        }
+        endTime = System.nanoTime();
+        System.out.println("Время заполнения хеш-таблицы методом линейного пробирования: " + (endTime - startTime) + " нс");
+
+        hashTable.insert(new Item(777));
+        hashTable.insert(new Item(123));
+        hashTable.insert(new Item(123));   // Коллизия
+        hashTable.display();
+
+        System.out.println(hashTable.find(777).getKey());
+        hashTable.delete(123);
 
 
     }
